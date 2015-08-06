35f675e4bec898aade267bb9c905d6e0ca123e31
haotie1990 <haotie1990@foxmail.com>
2015-08-06 17:13:28 +0800

    添加导航条

--- app/src/main/java/com/tv/remote/activity/TvRemoteActivity.java
+++ app/src/main/java/com/tv/remote/activity/TvRemoteActivity.java
@@ -2,18 +2,38 @@ package com.tv.remote.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.tv.remote.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by 凯阳 on 2015/8/6.
  */
 public class TvRemoteActivity extends AppCompatActivity{
 
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainlayout);
+
+        ButterKnife.inject(this);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(null);
+            getSupportActionBar().setSubtitle(null);
+            toolbar.setNavigationIcon(R.drawable.btn_option);
+        }
     }
 
     @Override
--- app/src/main/java/com/tv/remote/app/TvRemoteApplication.java
+++ app/src/main/java/com/tv/remote/app/TvRemoteApplication.java
@@ -1,7 +1,14 @@
 package com.tv.remote.app;
 
+import android.app.Application;
+
 /**
  * Created by 凯阳 on 2015/8/6.
  */
-public class TvRemoteApplication {
+public class TvRemoteApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
